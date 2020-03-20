257c7a4b25d952911293111fd786a3e279d7fb5f
SlezaKlouna <knijnaya.lakomka83@gmail.com>
2020-03-20 13:10:39 +0200

    Error routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./vuex/store"
+import router from "./router/router"
 import './assets/styles/style.scss'
 
 
@@ -9,4 +10,5 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   store,
+  router
 }).$mount('#app')
